d6b28a45af34185b0b041fefeea537b10d24ee83
Robert Brownstein <robert.brownstein@gmail.com>
2025-02-05 23:13:06 -0500

    Make site less awful

--- src/App.tsx
+++ src/App.tsx
@@ -1,111 +1,80 @@
-import debounce from 'debounce';
-import { useState, useEffect } from 'react';
-import { Background } from './Background';
-import { Bird } from './Birdify';
-import './App.css';
-import resumeThumbnail from './resume-thumbnail.png';
-import osThumbnail from './open-sourcerer.png';
+import { useState, useEffect } from "react";
+import openSourcererPng from "./os-screenshot.png";
 
-const MOBILE_CUTOFF = 900;
+import "./App.css";
 
 function App() {
-  const [birdActive, setBirdActive] = useState<boolean>(false);
-  const [mobile, setMobile] = useState<boolean>(window.innerWidth < MOBILE_CUTOFF);
-  const [bgColor, setBgColor] = useState<[number, number, number]>([0.9, 0.9, 0.9]);
-  useEffect(() => {
-    let active = true;
-    function resize() {
-      setMobile(window.innerWidth < MOBILE_CUTOFF);
+  const [expanded, setExpanded] = useState<"game" | "links" | null>(null);
+
+  const toggle = (section: typeof expanded) => {
+    if (expanded === section) {
+      setExpanded(null);
+    } else {
+      setExpanded(section);
     }
-    function pulse() {
-      if (!active) {
-        return;
-      }
-      setBgColor([
-        Math.random(),
-        Math.random(),
-        Math.random()
-      ]);
-      setTimeout(pulse, 10000);
-    }
-    setTimeout(pulse, 1000);
-    const debouncedResize = debounce(resize, 200);
-    window.addEventListener('resize', debouncedResize);
-    return () => {
-      active = false;
-      window.removeEventListener('resize', debouncedResize);
-    };
-  }, [setBgColor]);
-  useEffect(() => {
-    function activateBird() {
-      if (!birdActive) {
-        setBirdActive(true);
-      }
-    }
-    const birdTimeout = setTimeout(activateBird, 1500);
-    return () => {
-      clearTimeout(birdTimeout);
-    };
-  }, [setBirdActive, birdActive]);
-  const colorTheme = (bgColor[0] + bgColor[1] * 2 + bgColor[2] * 0.5 > 1.5)
-    ? 'light'
-    : 'dark';
-  const tile1 = (
-    <div className='tile borderless'>
-      <h2>Full Stack Software Engineer</h2>
-    </div>
-  );
-  const tile2 = (
-    <div className='tile borderless'>
-      <h3>I'm fluent in TypeScript, Python, WebGL, React, Node.js, AWS, Docker, SQL, and NoSQL. I'm passionate about realtime spatial problems and great UX.</h3>
-    </div>
-  );
-  const tile3 = (
-    <div className='tile borderless'>
-      <h3>Recent Employment</h3>
-      <ul>
-        <li>2022 - Current: <b>Boston Dynamics</b> ( IC )</li>
-        <li>2019 - 2022: <b>Better</b> ( IC )</li>
-        <li>2016 - 2019: <b>Social Tables</b> ( IC / EM )</li>
-      </ul>
-      <h3>Open Source</h3>
-      <ul>
-        <li><a href='https://brownstein.github.io/three-aseprite/' target='_blank'>Three-Aseprite</a></li>
-        <li><a href='https://github.com/socialtables/openfpc' target='_blank'>OpenFPC</a></li>
-        <li><a href='https://github.com/socialtables/saml-protocol' target='_blank'>SAML-Protocol</a></li>
-      </ul>
-    </div>
-  );
+  };
+
   return (
-    <div className={`app ${colorTheme}`}>
-      <Background color={bgColor}/>
-      <div className='content-box'>
-        <h1>Robert Brownstein { mobile ? null : <Bird active={birdActive}/>}</h1>
-        { mobile ? <div className='row'>{tile1}{tile2}</div> : null }
-        { mobile ? (
-          <div className='bird-row'>
-            <Bird active={birdActive}>{'{}'}</Bird>
-            <Bird active={birdActive}>{'()'}</Bird>
-            <Bird active={birdActive}>{'[]'}</Bird>
+    <>
+      <div className={`headline ${expanded ? "small" : "fullsize"}`}>
+        <h1>Robert Brownstein</h1>
+        <h2>( Software Engineer )</h2>
+      </div>
+      <div className="content">
+        <a
+          className="link-large link-resume"
+          href="Robert%20Brownstein%20Resume.pdf"
+          target="_blank"
+        >
+          <div className="preview" />
+          <div className="label">Resume</div>
+        </a>
+        <div
+          className={`link-large link-game ${
+            expanded === "game" ? "expanded" : "minimized"
+          }`}
+        >
+          <a className="preview" onClick={() => toggle("game")} />
+          <div className="link-content">
+            <img alt="Screenshot from Open Sourcerer" src={openSourcererPng} />
+            <p>
+              Open Sourcerer, developed by Gnarled Helix LLC, is a 2D platformer
+              in which the player learns programming to customize their spells.
+            </p>
+            <p>
+              I deploy a playable build{" "}
+              <a href="https://brownstein.github.io/osts-v3/">here</a> from time
+              to time.
+            </p>
           </div>
-        ) : null }
-        <div className='row'>
-          <a className='tile big resume' href="Robert%20Brownstein%20Resume.pdf" target='_blank'>
-            <img src={resumeThumbnail} />
-            <span className='label'>Download Resume Here.</span>
-          </a>
-          <a className='tile big' href='https://brownstein.github.io/open-sourcerer-ts' target='_blank'>
-            <img src={osThumbnail} />
-            <div className='description'>
-              Open Sourcerer is a 2D platformer in which the player learns JavaScript to defeat enemies and solve puzzles. Designed for desktop browsers.
-            </div>
-            <span className='label'>Check out my Side Project Here.</span>
+          <a className="label" onClick={() => toggle("game")}>
+            Video Game
           </a>
         </div>
-        { mobile ? null : <div className='flex-row'>{tile1}{tile2}{tile3}</div> }
-        { mobile ? <div className='row'>{tile3}</div> : null }
+        <div
+          className={`link-large link-links ${
+            expanded === "links" ? "expanded" : "minimized"
+          }`}
+        >
+          <a className="preview" onClick={() => toggle("links")} />
+          <div className="link-content">
+            <ul>
+              <li>
+                <a href="https://brownstein.github.io/three-aseprite/">
+                  <div className="link-headline">Three-Asperite</div>
+                  <div className="link-description">
+                    (open source rendering package)
+                  </div>
+                </a>
+              </li>
+            </ul>
+          </div>
+          <a className="label" onClick={() => toggle("links")}>
+            Links
+          </a>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
